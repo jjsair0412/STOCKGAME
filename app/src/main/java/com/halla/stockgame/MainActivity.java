f62965a77ae121d16a92825704e3cb46d9fb8b28
DESKTOP-5B9VU1L\skine <skine134@naver.com>
2021-05-18 00:16:04 +0900

    activity_main viewbinding 패턴 변환.

--- app/src/main/java/com/halla/stockgame/MainActivity.java
+++ app/src/main/java/com/halla/stockgame/MainActivity.java
@@ -6,24 +6,22 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
+import com.halla.stockgame.databinding.ActivityMainBinding;
 import com.halla.stocklist_fragment.Stockinfo;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button StartBtn;
-    Button HelpBtn;
-    Button QuitBtn;
-    Button HaveNoSidemoney;
-
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         DBHelper dbHelper = new DBHelper(getApplicationContext(), "SidMoney", null, 1);
 
@@ -31,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         boolean checkFirst = pref.getBoolean("checkFirst",true);
 
 
-        if(checkFirst == true) { // 어플리케이션이 최초로 실행되었을 경우에만 수행하는 if문
+        if(checkFirst) { // 어플리케이션이 최초로 실행되었을 경우에만 수행하는 if문
 
             SharedPreferences.Editor editor2 = pref.edit();
             dbHelper.insert("MainUser",10000);
@@ -46,8 +44,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d("실험123123", "onCreate: " + dbHelper.getResult());
 
 
-        HaveNoSidemoney = (Button) findViewById(R.id.HaveNoSidemoney);
-        HaveNoSidemoney.setOnClickListener(new View.OnClickListener() {
+        binding.HaveNoSidemoney.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dbHelper.update("MainUser",10000);
@@ -56,8 +53,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        StartBtn = (Button) findViewById(R.id.StartBtn);
-            StartBtn.setOnClickListener(new View.OnClickListener() { // 게임시작 버튼 . 주가정보 나오는 엑티비티로 이동
+            binding.StartBtn.setOnClickListener(new View.OnClickListener() { // 게임시작 버튼 . 주가정보 나오는 엑티비티로 이동
                 @Override
                 public void onClick(View view) {
 
@@ -68,8 +64,7 @@ public class MainActivity extends AppCompatActivity {
             });
 
 
-            HelpBtn = (Button) findViewById(R.id.HelpBtn);
-            HelpBtn.setOnClickListener(new View.OnClickListener() { //도움말 버튼
+            binding.HelpBtn.setOnClickListener(new View.OnClickListener() { //도움말 버튼
                 @Override
                 public void onClick(View view) {
 
@@ -77,8 +72,7 @@ public class MainActivity extends AppCompatActivity {
             });
 
 
-            QuitBtn = (Button) findViewById(R.id.QuitBtn);
-            QuitBtn.setOnClickListener(new View.OnClickListener() { // 게임종료 버튼
+            binding.QuitBtn.setOnClickListener(new View.OnClickListener() { // 게임종료 버튼
                 @Override
                 public void onClick(View view) {
 
